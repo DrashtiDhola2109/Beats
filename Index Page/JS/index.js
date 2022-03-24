6e1bc8df925f4b00e9810faef130b8f36812b8d1
Archit Ghevariya <ghevariyaarchit@gmail.com>
2022-03-25 02:29:21 +0530

    Music player and Songs Added

--- Index Page/JS/index.js
+++ Index Page/JS/index.js
@@ -1,1 +1,6 @@
 'use strict';
+
+function openplayer()
+{
+    window.open("../HTML/player.html","_self")
+}
---
+++ Index Page/JS/songs.js
@@ -0,0 +1,51 @@
+let songs = [
+    {
+      name: "All to Well",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Tylor Swift",
+      audio: "https://a1moviez4u1.logan7.workers.dev/1:/Music/Y2Mate.is%20-%20All%20Too%20Well%20(10%20Minute%20Version)%20(Taylor's%20Version)%20(From%20The%20Vault)%20(Lyric%20Video)-sRxrwjOtIag-160k-1643204806926.mp3",
+      id: 0,
+    },
+    {
+      name: "After Glow",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Ed Sheeran",
+      audio: "https://a1moviez4u1.logan7.workers.dev/1:/Music/Ed-Sheeran-Afterglow-Official.mp3",
+      id: 1,
+    },
+    {
+      name: "No Time To Die",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Billie Eilish",
+      audio: "https://a1moviez4u1.logan7.workers.dev/1:/Music/Y2Mate.is%20-%20Billie%20Eilish%20-%20No%20Time%20To%20Die-BboMpayJomw-160k-1642761798827.mp3",
+      id: 2,
+    },
+    {
+      name: "Bad Habits",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Ed Sheeran",
+      audio: "https://a1moviez4u1.logan7.workers.dev/1:/Music/Ed-Sheeran-Bad-Habits.mp3",
+      id: 3,
+    },
+    {
+      name: "Put it all on me",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Hakim Omari4",
+      audio: "https://a1moviez4u1.logan7.workers.dev/1:/Music/Ed-Sheeran-Ft-Ella-Mai-Put-It-All-On-Me.mp3",
+      id: 4,
+    },
+    {
+      name: "Lovely",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Khalid",
+      audio: "https://a1moviez4u1.logan7.workers.dev/1:/Music/OnlyMP3.net%20-%20Billie%20Eilish,%20Khalid%20-%20lovely-V1Pl8CzNzCw-192k-1633398695359.mp3",
+      id: 5,
+    },
+  ];
+ 
